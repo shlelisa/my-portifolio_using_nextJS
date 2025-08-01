fd723797f31491de40f13604ebd17cacd6a28288
shlelisa <shlelisa@gmail.com>
2025-08-01 14:34:03 +0300

    removing neon database

--- app/CustomerSide/Blogs/page.tsx
+++ app/CustomerSide/Blogs/page.tsx
@@ -2,55 +2,39 @@
 
 import React, { useEffect, useState } from "react";
 
-import { neon } from "@netlify/neon";
-
-const sql = neon(); // automatically use netlify database url
 
 const Blogs = () => {
-  const [posts, setPosts] = useState<Record<string, string>[]>([]);
 
-  useEffect(() => {
-    async function fetchPosts() {
-      try {
-        const rows = await sql`SELECT * FROM posts ORDER BY created_at DESC`;
-        setPosts(rows);
-      } catch (error) {
-        console.error("Error fetching posts:", error);
-      }
-    }
 
-    fetchPosts();
-  }, []);
-
-//   const blogPosts = [
-//     {
-//       id: 1,
-//       title: "Getting Started with Web Development",
-//       summary:
-//         "An introductory guide to HTML, CSS, and JavaScript for aspiring developers.",
-//       author: "Lelisa Shashura",
-//       date: "July 28, 2025",
-//       image: "/assets/blog1.jpg",
-//     },
-//     {
-//       id: 2,
-//       title: "Why React is a Game Changer",
-//       summary:
-//         "Explore the benefits of React and how it simplifies frontend development.",
-//       author: "Lelisa Shashura",
-//       date: "July 24, 2025",
-//       image: "/assets/blog2.jpg",
-//     },
-//     {
-//       id: 3,
-//       title: "Deploying Websites with Vercel",
-//       summary:
-//         "Learn how to deploy your React or Next.js apps using Vercel in minutes.",
-//       author: "Lelisa Shashura",
-//       date: "July 20, 2025",
-//       image: "/assets/blog3.jpg",
-//     },
-//   ];
+  const blogPosts = [
+    {
+      id: 1,
+      title: "Getting Started with Web Development",
+      summary:
+        "An introductory guide to HTML, CSS, and JavaScript for aspiring developers.",
+      author: "Lelisa Shashura",
+      date: "July 28, 2025",
+      image: "/assets/blog1.jpg",
+    },
+    {
+      id: 2,
+      title: "Why React is a Game Changer",
+      summary:
+        "Explore the benefits of React and how it simplifies frontend development.",
+      author: "Lelisa Shashura",
+      date: "July 24, 2025",
+      image: "/assets/blog2.jpg",
+    },
+    {
+      id: 3,
+      title: "Deploying Websites with Vercel",
+      summary:
+        "Learn how to deploy your React or Next.js apps using Vercel in minutes.",
+      author: "Lelisa Shashura",
+      date: "July 20, 2025",
+      image: "/assets/blog3.jpg",
+    },
+  ];
 
   return (
     <div className="max-w-6xl mx-auto px-4 py-20">
@@ -58,10 +42,10 @@ const Blogs = () => {
         Our Latest Blogs
       </h2>
 
-      {posts.length === 0 ? (
+      {blogPosts.length === 0 ? (
         <p> No Posts found.</p>
       ) : (
-        posts.map((post) => (
+        blogPosts.map((post) => (
           <div  key={post.id} className="grid grid-cols-1 md:grid-cols-3 gap-8">
             <div
              
@@ -91,7 +75,7 @@ const Blogs = () => {
         ))
       )}
     </div>
-  );
-};
+  )}
+
 
 export default Blogs;
